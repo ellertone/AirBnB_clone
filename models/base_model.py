4a551563cdefa25280e66e4a218bfe022790e862
Ellertone <ellertonemain@gmail.com>
2023-08-11 21:54:44 +0300

    base_model complete

--- models/base_model.py
+++ models/base_model.py
@@ -26,14 +26,23 @@ class BaseModel:
             for k, v in kwargs.items:
                 if k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strftime(format_time)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update time to the current"""
         self.updated_at = datetime.now()
-        return self.updated_at
+        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary of the Basemodel Class"""
+        a_dict = self.__dict__.copy()
+        a_dict["created_at"] = self.created_at
+        a_dict["updated_at"] = self.updated_at
+        a_dict["__class__"] = self.__class__.__name__
+        return a_dict
 
     def __str__(self):
         """Print out the string representation of the class"""
